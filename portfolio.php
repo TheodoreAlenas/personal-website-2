91fd339462d59258f50f7d68ba17e1f97809ad3a
Theodore Alenas <dimakopt732@gmail.com>
2023-11-14 21:46:42 +0200

    prettier snippet colors and snippet framework

--- portfolio.php
+++ portfolio.php
@@ -3,6 +3,15 @@
         global $EN, $GR; $EN($array->en); $GR($array->gr);
     }
     $a = "portfolio_array_to_en_gr";
+
+    function encode_and_surround_with_b_tag($strings, $code) {
+        $code = htmlspecialchars($code);
+        foreach ($strings as $string) {
+            $code = str_replace($string, "<b>$string</b>", $code);
+        }
+        echo $code;
+    }
+    $b = "encode_and_surround_with_b_tag";
 ?>
 
 
@@ -22,123 +31,162 @@
 <p>
     A system to provide a listing of key bindings and optional
     input box to key bindings, built around
-    <code>sxhkd</code>, with 4 major reworks:
-    <ol>
-        <li>
-            <h3>alchain</h3>
-            <p>
-                3 programs,
-                <code>alchain</code>,
-                <code>alpopup</code> and
-                <code>hk-tui.bash</code>,
-                working together so that when pressing the Windows key
-                a terminal appears at the bottom of the screen,
-                full width, displays possible
-                key bindings and waits for key input.
-                After pressing Windows, getting the terminal,
-                and pressing another key such as <code>W</code>,
-                often nothing would have happened yet and the terminal
-                would be showing next possible key presses.
-            </p>
-            <pre><b>read</b> -n 1 -s -r
-next=$( <b>awk</b> "/^$REPLY/ { print \$2 }" hk-states/$state )
+    <code>sxhkd</code>, with 4 major versions:
+</p>
+<h3>alchain</h3>
+<p>
+    3 programs,
+    <code>alchain</code>,
+    <code>alpopup</code> and
+    <code>hk-tui.bash</code>,
+    working together so that when pressing the Windows key
+    a terminal appears at the bottom of the screen,
+    full width, displays possible
+    key bindings and waits for key input.
+    After pressing Windows, getting the terminal,
+    and pressing another key such as <code>W</code>,
+    often nothing would have happened yet and the terminal
+    would be showing next possible key presses.
+</p>
+<pre><?php
+    $b(["read", "next", '$REPLY', '$state'],
+    <<<EOSH
+read -n 1 -s -r
+next=$( awk "/^\$REPLY/ { print \\\$2 }" hk-states/\$state )
 
-<b>if</b> [ -z $next ]
-<b>then</b>
+if [ -z \$next ]
+then
     next=ROOT
-    <b>bspc</b> node last --focus
-<b>fi</b></pre>
-            <i>part of <code>hk-tui.bash</code></i>
-            <p>
-                The flaw was that windows are not designed to work
-                well when treated like this.
-                There were more than just bugs.
-                Windows play a more decorative role than anticipated.
-            </p>
-        </li>
-        <li>
-            <h3>alk</h3>
-            <p>
-                Shell scripts that take arguments corresponding
-                to hotkeys, and perform the actions.
-                The <code>al</code> executable displays
-                the <code>stdout</code> and <code>stderr</code>
-                in a terminal,
-                if it was called from an environment without
-                the <code>INTERACTIVE</code> variable set.
-                The shell function definitions of the <code>alk-*</code>
-                executables compile into the help message
-                which acts like a listing of key bindings.
-                While the code may look weird, it's close to
-                how I write shell scripts today. Note that
-                it's POSIX compliant instead of Bash.
-            </p>
-            <pre><b>if</b> [ "$INTERACTIVE" = '' ]
-<b>then</b> set_interactive
-<b>fi</b>
+    bspc node last --focus
+fi
+EOSH);
+?></pre>
+<i>part of <code>hk-tui.bash</code></i>
+<p>
+    The flaw was that windows are not designed to work
+    well when treated like this.
+    There were more than just bugs.
+    Windows play a more decorative role than anticipated.
+</p>
+<h3>alk</h3>
+<p>
+    Shell scripts that take arguments corresponding
+    to hotkeys, and perform the actions.
+    The <code>al</code> executable displays
+    the <code>stdout</code> and <code>stderr</code>
+    in a terminal,
+    if it was called from an environment without
+    the <code>INTERACTIVE</code> variable set.
+    The shell function definitions of the <code>alk-*</code>
+    executables compile into the help message
+    which acts like a listing of key bindings.
+    While the code is weird, it's much closer to
+    how I write shell scripts today. Note that
+    it's POSIX compliant instead of Bash.
+</p>
+<pre><?php
+    $b(["set_interactive", "alk-router", "execute_al "],
+    <<<EOSH
+if [ "\$INTERACTIVE" = '' ]
+then set_interactive
+fi
 
 execute_al() {
-    <b>alk-router</b> "$@"
+    alk-router "\$@"
 }
 execute_al_interactive() {
-    <b>execute_al</b> "$@"
-}</pre>
-            <i>part of <code>al</code></i>
-            <p>
-                Compared to the first approach, this one had
-                very verbose configuration files.
-                Also, it was never actually used in the terminal.
-            </p>
-        </li>
-        <li>
-            <h3>alsnip</h3>
-            <p>
-                A code snippet generator, that can take multiple
-                inputs to complete parts of a snippet,
-                and provides auto completion in the process.
-                The snippets could be shell scripts that
-                would run, and the completion could be started
-                by pressing a key binding.
-                Built in, the configuration expressed
-                which snippets were bound to keys,
-                and which of them needed terminal interactions
-                such as entering passwords.
-                It was almost ready to be published,
-                but it wasn't self-contained at all,
-                plus I abandoned it before publishing it.
-            </p>
-            <pre>fn=powff+key+is-e-q+key-term:"$fn"
+    execute_al "\$@"
+}
+EOSH);
+?></pre>
+<i>part of <code>al</code></i>
+<p>
+    Compared to the first approach, this one had
+    very verbose configuration files.
+    Also, it was never actually used in the terminal.
+</p>
+<h3>alsnip</h3>
+<p>
+    A code snippet generator, that can take multiple
+    user inputs to complete parts of a snippet,
+    and provides auto completion in the process.
+    The snippets could be shell scripts that
+    would run, and the completion could be started
+    by pressing a key binding.
+    Built in, the configuration expressed
+    which snippets were shell scripts (to be ran),
+    and of them, which were bound to keys,
+    and which needed terminal interactions
+    such as entering passwords.
+    It was almost ready to be published,
+    but it wasn't self-contained at all,
+    plus I abandoned it before publishing it.
+</p>
+<pre><?php
+    $b(["powff", "script", "descr", "arg1"],
+    <<<EOSH
+fn=powff+key+is-e-q+key-term:"\$fn"
 fn_powff() {
-  <b>case</b> "$act" in
-    script) printf '<b>sudo shutdown %s\n</b>' "$1" ;;
-    descr) cat &lt;&lt;EOM
-<b>systemd command,  poweroff  is an alias to  shutdown now</b>
+  case "\$act" in
+    script) printf 'sudo shutdown %s\\n' "\$1" ;;
+    descr) cat <<EOM
+systemd command,  poweroff  is an alias to  shutdown now
 EOM
     ;;
-    arg1) printf "<b>recommend\nnow\n+1\n+5\n+30\n23:59\n02:00\n</b>" ;;
-  <b>esac</b>
-}</pre>
-            <i>part of <code>example-config/reset.sh</code></i>
-            <p>
-                I didn't ever use snippets, nor did I start using
-                snippets then. So it still was a hotkey system only.
-                It abused Robert C. Martin's insights too, making it
-                one of the most complicated pieces of software
-                I had made by then.
-            </p>
-        </li>
-        <li>
-            <h3>scmd</h3>
-            <p>A single file.</p>
-            <pre><b>volume_set_8</b>() { pacmd set-sink-volume 0 48000; } <b>#m8</b>
+    arg1) printf "recommend\\nnow\\n+1\\n+5\\n+30\\n23:59\\n02:00\\n" ;;
+  esac
+}
+EOSH);
+?></pre>
+<i>part of <code>example-config/reset.sh</code></i>
+<p>
+    I didn't ever use snippets, nor did I start using
+    snippets then. So it still was a hotkey system only.
+    It abused Robert C. Martin's insights too, making it
+    one of the most complicated pieces of software
+    I had made by then.
+</p>
+<h3>scmd</h3>
+<p>A single file.</p>
+<pre><b>volume_set_8</b>() { pacmd set-sink-volume 0 48000; } <b>#m8</b>
 <b>volume_set_9</b>() { pacmd set-sink-volume 0 54000; } <b>#m9</b>
 <b>volume_set_custom</b>() { pacmd set-sink-volume 0 "$(:|dmenu)"; }
 
 <b>wallpaper_set_scale</b>() { bspc_float; feh --bg-scale "$(find ~/l/gwp/ -type f -print0 | xargs -0 sxiv -ot)"; }
 <b>wallpaper_set_fill</b>()  { bspc_float; feh --bg-fill  "$(find ~/l/gwp/ -type f -print0 | xargs -0 sxiv -ot)"; }</pre>
-        </li>
-    </ol>
+<p>
+    Every line is either blank, a comment,
+    or an inline shell function definition.
 </p>
+<pre><?php
+    $b(["scmd_run", "this_file", "scmd_with_bar_status", "#x"],
+    <<<EOSH
+scmd_run() { c="\$(dmenu < "\$(this_file)" | cut -d'(' -f1)"; test "\$c" && scmd_with_bar_status "\$c"; } #x
+  [...]
+this_file() { echo ~/.config/scmd.sh; }
+EOSH);
+?></pre>
+<p>
+    This is the product that I'm most proud of as of now, November 2023.
+    Nobody can understand why I'm proud of a single file
+    and if my past self saw it, he'd want to make it better by
+    ruining it. It doesn't matter, nothing matters, I'm here to
+    appreciate it as much as I deserve, and as much as my computer
+    deserves, because this work is impossible for a person to do alone,
+    this work is collaboration between a person and a computer
+    giving feedback over what software makes it feel healthy
+    and what software burdens it.
+</p>
+<pre><?php
+    $b(["ls_keys", "sxhkd", "awk ", "awk1", "awk2", "this_file"],
+    <<<EOSH
+scmd_ls_keys() { sed -n 's/^\\([a-z0-9_]\\+\\)(.*#\\([^}]*\\)\$/\\2 \\1/p' "\$(this_file)"; }
+scmd_sxhkdrc_vim() { in_vim scmd_sxhkdrc; }
+scmd_sxhkdrc() { scmd_ls_keys | awk "{ \$(scmd_awk1) \$(scmd_awk2) }"; }
+scmd_awk1() { printf %s 's = \$1; gsub(/./, "; super + &", s); sub("; ", "", s); '; }
+scmd_awk2() { printf %s 'print(s); printf("\\t. %s && scmd_with_bar_status %s\\n\\n", "'"\$(this_file)"'", \$2);'; }
+EOSH);
+?></pre>
 
 <?php
     $unused_variable_for_holding_license =
