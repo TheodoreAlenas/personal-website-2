3e9c31d2067d040564bef16fb8d678dc5af5bf2f
Theodore Alenas <dimakopt732@gmail.com>
2023-12-01 10:32:37 +0200

    realized ul must be outside p

--- home/home.php
+++ home/home.php
@@ -10,22 +10,18 @@
 <h1><?php $a($h->teo); echo ' '; $a($h->dimak); ?></h1>
 
 <p><?php $a($h->caring_programmer) ?></p>
-<p>
-    <?php $a($h->i_trust) ?>
-    <ul>
-        <li><?php $a($h->t1) ?></li>
-        <li><?php $a($h->t2) ?></li>
-        <li><?php $a($h->t3) ?></li>
-    </ul>
-</p>
-<p>
-    <?php $a($h->i_follow_the_rules) ?>
-    <ul>
-        <li><?php $a($h->r1) ?></li>
-        <li><?php $a($h->r2) ?></li>
-        <li><?php $a($h->r3) ?></li>
-    </ul>
-</p>
+<p><?php $a($h->i_trust) ?></p>
+<ul>
+    <li><?php $a($h->t1) ?></li>
+    <li><?php $a($h->t2) ?></li>
+    <li><?php $a($h->t3) ?></li>
+</ul>
+<p><?php $a($h->i_follow_the_rules) ?></p>
+<ul>
+    <li><?php $a($h->r1) ?></li>
+    <li><?php $a($h->r2) ?></li>
+    <li><?php $a($h->r3) ?></li>
+</ul>
 <p><?php $a($h->lets_go_out) ?></p>
 
 <?php
